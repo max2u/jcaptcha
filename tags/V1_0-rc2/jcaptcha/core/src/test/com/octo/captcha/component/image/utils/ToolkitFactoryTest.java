7cac3849037ee701b7862c54ccdb78bd7bde539b
nobody <nobody@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2004-11-21 19:28:56 +0000

    This commit was manufactured by cvs2svn to create tag 'V1_0-rc2'.

---
+++ tags/V1_0-rc2/jcaptcha/core/src/test/com/octo/captcha/component/image/utils/ToolkitFactoryTest.java
@@ -0,0 +1,54 @@
+package com.octo.captcha.component.image.utils;
+
+import junit.framework.TestCase;
+
+import java.awt.*;
+
+/**
+ * <p>Description: </p>
+ *
+ * @author <a href="mailto:mga@octo.com">Mathieu Gandin</a>
+ * @version 1.0
+ */
+public class ToolkitFactoryTest extends TestCase {
+
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    /**
+     * Constructor for ToolkitFactoryTest.
+     *
+     * @param name
+     */
+    public ToolkitFactoryTest(String name) {
+        super(name);
+    }
+
+    public void testGetaDefaultToolkit() {
+        assertTrue(ToolkitFactory.getToolkit() instanceof Toolkit);
+    }
+
+    public void testGetCustomToolkit(){
+        System.setProperty(ToolkitFactory.TOOLKIT_IMPL,MockToolkit.class.getName());
+        assertTrue(ToolkitFactory.getToolkit() instanceof MockToolkit);
+        System.setProperty(ToolkitFactory.TOOLKIT_IMPL,"toto");
+        try
+        {
+            ToolkitFactory.getToolkit();
+            fail("should throw an exception");
+        } catch (Exception e)
+        {
+            //assertTrue();
+        }
+
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+         System.setProperty(ToolkitFactory.TOOLKIT_IMPL,Toolkit.getDefaultToolkit().getClass().toString());
+        
+    }
+}
