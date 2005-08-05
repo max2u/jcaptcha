d85d6e9f5fd83c91765adfee8557392f7293ae03
magarrigue <magarrigue@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2005-08-05 16:23:42 +0000

    javadoc and formating

--- trunk/jcaptcha/core/src/java/com/octo/captcha/Captcha.java
+++ trunk/jcaptcha/core/src/java/com/octo/captcha/Captcha.java
@@ -473,17 +473,17 @@ import java.io.Serializable;
  * humans can pass.</li> <li>Current computer programs can't pass</li> </ul> see
  * http://www.captcha.net/ for sample, articles, and definitions.
  * <p/>
- * A capchta is basically a test composed of : <ul> <li>A question about :</li>
+ * A captcha is basically a test composed of : <ul> <li>A question about :</li>
  * <li>a challenge (can be an image for image captchas or a sound, or
  * wathever)</li> <li>a validation routine a of a given response</li> </ul>
  * <p/>
  * This is a container for the CAPTCHA challenge which is also able to validate
  * the answer. Class implementing this interface must follow the rules : <ul>
- * <li>As all 'components' of this project, it must have a single
+ * <li>As all 'components' of this project, all contructed objects should be abble to word (ie no need to set other properties to obtain a fully fonctional object).
  * constructor</li> <li>It must not build the challenge! use instead the
  * CaptchaFactory</li> <li>It must know how to validate the answer</li> <li>It
  * must not expose the answer</li> <li>It must dispose the challenge when the
- * getChallenge method is called(The challenge must be showed only once and)
+ * getChallenge method is called(The challenge must be showed only once )
  * </li> </ul> ;
  *
  * @author <a href="mailto:mag@jcaptcha.net">Marc-Antoine Garrigue</a>
