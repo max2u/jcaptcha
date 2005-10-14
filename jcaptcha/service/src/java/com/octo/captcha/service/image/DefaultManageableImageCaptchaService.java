bec682977e0abc9462d6f155c93dd739c2aaabd0
magarrigue <magarrigue@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2005-10-14 15:29:22 +0000

    FWK-21 Bug in EHCacheCaptchaStore implemetation/integration

--- jcaptcha/service/src/java/com/octo/captcha/service/image/DefaultManageableImageCaptchaService.java
+++ jcaptcha/service/src/java/com/octo/captcha/service/image/DefaultManageableImageCaptchaService.java
@@ -8,27 +8,50 @@ package com.octo.captcha.service.image;
 
 
 import com.octo.captcha.engine.image.gimpy.DefaultGimpyEngine;
+import com.octo.captcha.engine.CaptchaEngine;
+import com.octo.captcha.service.captchastore.FastHashMapCaptchaStore;
+import com.octo.captcha.service.captchastore.CaptchaStore;
 
 /**
- * <p>Default service implementation : use a ehCache as captcha store, and a DefaultGimpyEngine </p> It is initialized
+ * <p>Default service implementation : use a {@link FastHashMapCaptchaStore} as captcha store, and a DefaultGimpyEngine </p> It is initialized
  * with thoses default values : <ul> <li>min guaranted delay : 180s </li> <li>max store size : 100000 captchas </li>
- * <li>max store size before garbage collection : non applicable </li> </ul>
+ * <li>max store size before garbage collection : 75000 </li> </ul>
  *
  * @author <a href="mailto:mag@jcaptcha.net">Marc-Antoine Garrigue</a>
  * @version 1.0
  */
-public class DefaultManageableImageCaptchaService extends EhcacheManageableImageCaptchaService
+public class DefaultManageableImageCaptchaService extends AbstractManageableImageCaptchaService
         implements ImageCaptchaService {
-
+    /**
+     * Construct a new ImageCaptchaService with a {@link FastHashMapCaptchaStore} and a {@link DefaultGimpyEngine}
+     *  minGuarantedStorageDelayInSeconds = 180s
+     *  maxCaptchaStoreSize = 100000
+     *  captchaStoreLoadBeforeGarbageCollection=75000
+     */
     public DefaultManageableImageCaptchaService() {
-        super(new DefaultGimpyEngine(), 180,
-                100000, "image");
+        super(new FastHashMapCaptchaStore(), new DefaultGimpyEngine(), 180,
+                100000, 75000);
     }
 
+    /**
+     * Construct a new ImageCaptchaService with a {@link FastHashMapCaptchaStore} and a {@link DefaultGimpyEngine}
+     * @param minGuarantedStorageDelayInSeconds
+     * @param maxCaptchaStoreSize
+     * @param captchaStoreLoadBeforeGarbageCollection
+     */
+    public DefaultManageableImageCaptchaService( int minGuarantedStorageDelayInSeconds, int maxCaptchaStoreSize, int captchaStoreLoadBeforeGarbageCollection) {
+        super(new FastHashMapCaptchaStore(), new DefaultGimpyEngine(),minGuarantedStorageDelayInSeconds, maxCaptchaStoreSize, captchaStoreLoadBeforeGarbageCollection);
+    }
 
-    public DefaultManageableImageCaptchaService(String cacheName) {
-        super(new DefaultGimpyEngine(), 180,
-                100000, cacheName);
+    /**
+     * @param captchaStore
+     * @param captchaEngine
+     * @param minGuarantedStorageDelayInSeconds
+     * @param maxCaptchaStoreSize
+     * @param captchaStoreLoadBeforeGarbageCollection
+     */
+    public DefaultManageableImageCaptchaService(CaptchaStore captchaStore, CaptchaEngine captchaEngine, int minGuarantedStorageDelayInSeconds, int maxCaptchaStoreSize, int captchaStoreLoadBeforeGarbageCollection) {
+        super(captchaStore, captchaEngine, minGuarantedStorageDelayInSeconds, maxCaptchaStoreSize, captchaStoreLoadBeforeGarbageCollection);
     }
 
 
