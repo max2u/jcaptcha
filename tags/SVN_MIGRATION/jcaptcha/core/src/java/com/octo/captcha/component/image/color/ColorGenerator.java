7ac20b47207786b4b1a16bebe68bc7943b4f8acc
nobody <nobody@eaf2ec09-4c2e-0410-84d0-fe0bfca4863a>
2006-10-24 16:11:51 +0000

    This commit was manufactured by cvs2svn to create tag 'SVN_MIGRATION'.

---
+++ tags/SVN_MIGRATION/jcaptcha/core/src/java/com/octo/captcha/component/image/color/ColorGenerator.java
@@ -0,0 +1,25 @@
+/*
+ * jcaptcha, the open source java framework for captcha definition and integration
+ * Copyright (c) 2005 jcaptcha.net. All Rights Reserved.
+ * See the LICENSE.txt file distributed with this package.
+ */
+package com.octo.captcha.component.image.color;
+
+import java.awt.*;
+
+/**
+ * ColorGenerator is an interface used by TextPaster to generate color for rendering each character with specific
+ * color.
+ *
+ * @author Benoit Doumas
+ * @author Christian Blavier
+ */
+public interface ColorGenerator {
+    /**
+     * This return a new color, from a finite set.
+     *
+     * @return the next color
+     */
+    Color getNextColor();
+
+}
